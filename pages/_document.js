b420df47f702fb97aa149f20143116f0267a4cff
bare74 <bjornarenielsen@yahoo.no>
2024-05-04 08:18:46 +0200

    update

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import Test from "./test";
+// import Test from "./test";
 
 class MyDocument extends Document {
   render() {
@@ -32,7 +32,7 @@ class MyDocument extends Document {
         </Head>
         <body>
           <Main />
-          <Test />
+          {/* <Test /> */}
           <NextScript />
         </body>
       </Html>
